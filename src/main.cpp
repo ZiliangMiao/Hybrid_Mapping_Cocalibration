8d5a8803952a7f708cf0782c8d0a90816d645294
GODM26 <11911901@mail.sustech.edu.cn>
2022-06-09 20:30:34 +0800

    visualization.cpp created

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,8 @@
 // heading
 #include "imageProcess.h"
 #include "lidarProcess.h"
-#include "feature.h"
 #include "ceresAutoDiff.cpp"
-// #include "ceresOpt.cpp"
+#include "visualization.cpp"
 
 using namespace std;
 using namespace cv;
@@ -38,46 +37,7 @@ bool checkFolder(string FolderPath){
     return 1;
 }
 
-void fusionViz(imageProcess cam, string lidPath, vector< vector<double> > lidProjection, double bandwidth){
-    cv::Mat image = cam.readOrgImage();
-    int rows = image.rows;
-    int cols = image.cols;
-    cv::Mat lidarRGB = cv::Mat::zeros(rows, cols, CV_8UC3);
-    double pixPerRad = 1000 / (M_PI/2);
 
-    ofstream outfile;
-
-    outfile.open(lidPath, ios::out);
-
-    for(int i = 0; i < lidProjection[0].size(); i++){
-        double theta = lidProjection[0][i];
-        double phi = lidProjection[1][i];
-        // int u = (int)pixPerRad * theta;
-        // int v = (int)pixPerRad * phi;
-        int u = std::clamp(lidarRGB.rows - 1 - theta, (double)0.0, (double)(lidarRGB.rows-1));
-        int v = std::clamp(phi, (double)0.0, (double)(lidarRGB.cols-1));;
-        int b = 255;
-        int g = 0;
-        int r = 0;
-        lidarRGB.at<Vec3b>(u, v)[0] = b;
-        lidarRGB.at<Vec3b>(u, v)[1] = g;
-        lidarRGB.at<Vec3b>(u, v)[2] = r;
-        outfile << u << "," << v << endl;
-    }
-
-    outfile.close();
-
-    cv::Mat imageShow = cv::Mat::zeros(rows, cols, CV_8UC3);
-    cv::addWeighted(image, 1, lidarRGB, 0.8, 0, imageShow);
-    
-    std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> camResult = 
-        cam.fisheyeImageToSphere(imageShow);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPolarCloud;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPixelCloud;
-    std::tie(camOrgPolarCloud, camOrgPixelCloud) = camResult;
-    vector< vector< vector<int> > > camtagsMap = 
-        cam.sphereToPlane(camOrgPolarCloud, bandwidth);
-}
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "mainNode");
@@ -94,14 +54,13 @@ int main(int argc, char** argv){
 
     cout << "----------------- Camera Processing ---------------------" << endl;
     imageProcess imageProcess(pkgPath);
-
     imageProcess.setIntrinsic(params_calib);
-
     std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> camResult = imageProcess.fisheyeImageToSphere();
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPolarCloud;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPixelCloud;
     std::tie(camOrgPolarCloud, camOrgPixelCloud) = camResult;
     vector< vector< vector<int> > > camtagsMap = imageProcess.sphereToPlane(camOrgPolarCloud);
+    /********* Interrupt *********/
     vector< vector<int> > edgePixels = imageProcess.edgeToPixel();
     imageProcess.pixLookUp(edgePixels, camtagsMap, camOrgPixelCloud);
 
---
+++ src/visualization.cpp
@@ -0,0 +1,60 @@
+// basic
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+// opencv
+#include <opencv2/opencv.hpp>
+// ros
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <ros/package.h>
+// pcl
+#include <pcl/common/io.h>
+// heading
+#include "imageProcess.h"
+#include "lidarProcess.h"
+
+using namespace std;
+using namespace cv;
+
+void fusionViz(imageProcess cam, string lidPath, vector< vector<double> > lidProjection, double bandwidth){
+    cv::Mat image = cam.readOrgImage();
+    int rows = image.rows;
+    int cols = image.cols;
+    cv::Mat lidarRGB = cv::Mat::zeros(rows, cols, CV_8UC3);
+    double pixPerRad = 1000 / (M_PI/2);
+
+    ofstream outfile;
+
+    outfile.open(lidPath, ios::out);
+
+    for(int i = 0; i < lidProjection[0].size(); i++){
+        double theta = lidProjection[0][i];
+        double phi = lidProjection[1][i];
+        // int u = (int)pixPerRad * theta;
+        // int v = (int)pixPerRad * phi;
+        int u = std::clamp(lidarRGB.rows - 1 - theta, (double)0.0, (double)(lidarRGB.rows-1));
+        int v = std::clamp(phi, (double)0.0, (double)(lidarRGB.cols-1));;
+        int b = 255;
+        int g = 0;
+        int r = 0;
+        lidarRGB.at<Vec3b>(u, v)[0] = b;
+        lidarRGB.at<Vec3b>(u, v)[1] = g;
+        lidarRGB.at<Vec3b>(u, v)[2] = r;
+        outfile << u << "," << v << endl;
+    }
+
+    outfile.close();
+
+    cv::Mat imageShow = cv::Mat::zeros(rows, cols, CV_8UC3);
+    cv::addWeighted(image, 1, lidarRGB, 0.8, 0, imageShow);
+
+    std::tuple<pcl::PointCloud<pcl::PointXYZRGB>::Ptr, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> camResult =
+            cam.fisheyeImageToSphere(imageShow);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPolarCloud;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr camOrgPixelCloud;
+    std::tie(camOrgPolarCloud, camOrgPixelCloud) = camResult;
+    vector< vector< vector<int> > > camtagsMap =
+            cam.sphereToPlane(camOrgPolarCloud, bandwidth);
+}
