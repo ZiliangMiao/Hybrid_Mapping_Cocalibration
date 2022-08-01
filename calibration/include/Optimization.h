26514d1bdfb48bd875e9b6bc7360133c53a8ec12
GODM26 <11911901@mail.sustech.edu.cn>
2022-08-01 19:43:36 +0800

    headings modified.

---
+++ calibration/include/Optimization.h
@@ -0,0 +1,69 @@
+// basic
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <thread>
+// eigen
+#include <Eigen/Core>
+// ros
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <ros/package.h>
+// opencv
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+// ceres
+#include "ceres/ceres.h"
+#include "ceres/cubic_interpolation.h"
+#include "ceres/rotation.h"
+#include "glog/logging.h"
+// pcl
+#include <pcl/common/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Dense>
+// headings
+#include "FisheyeProcess.h"
+#include "LidarProcess.h"
+
+using namespace std;
+
+static const int kExtrinsics = 7;
+static const int kIntrinsics = 10;
+
+inline double getDouble(double x) {
+    return static_cast<double>(x);
+}
+
+template <typename SCALAR, int N>
+inline double getDouble(const ceres::Jet<SCALAR, N> &x) {
+    return static_cast<double>(x.a);
+}
+
+void Visualization2D(FisheyeProcess &fisheye, LidarProcess &lidar, std::vector<double> &params, double bandwidth);
+
+void Visualization3D(FisheyeProcess &fisheye, LidarProcess &lidar, std::vector<double> &params);
+
+std::vector<double> GradientCalib(FisheyeProcess &fisheye,
+                                LidarProcess &lidar,
+                                double bandwidth,
+                                std::vector<double> params_init);
+
+std::vector<double> QuaternionCalib(FisheyeProcess &fisheye,
+                                    LidarProcess &lidar,
+                                    double bandwidth,
+                                    std::vector<int> spot_vec,
+                                    std::vector<double> init_params_vec,
+                                    std::vector<double> lb,
+                                    std::vector<double> ub,
+                                    int kDisabledBlock);
+
+void CorrelationAnalysis(FisheyeProcess &fisheye,
+                        LidarProcess &lidar,
+                        std::vector<int> spot_vec,
+                        std::vector<double> params_vec);
